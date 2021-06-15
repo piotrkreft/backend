d2627987b6b2ad14c75a93d31ae4179990c9fa7c
Piotr Kreft <kreftpiotrek@gmail.com>
2021-06-15 10:14:34 +0200

    refactor: ViewEvent HTTP response handling

--- module/core/src/Application/HttpFoundation/FileContentResponse.php
+++ module/core/src/Application/HttpFoundation/FileContentResponse.php
@@ -7,33 +7,30 @@
 
 declare(strict_types=1);
 
-namespace Ergonode\Api\Application\Response;
+namespace Ergonode\Core\Application\HttpFoundation;
 
 use League\Flysystem\FilesystemInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * @deprecated
- */
-class FileContentResponse extends AbstractResponse
+class FileContentResponse extends Response
 {
-
     /**
-     * @throws \League\Flysystem\FileNotFoundException
+     * @throws \League\Flysystem\FileNotFoundException|\LogicException
      */
     public function __construct(string $filename, FilesystemInterface $storage)
     {
-        @trigger_error(
-            'Ergonode\Api\Application\Response\FileContentResponse is deprecated and will be removed in 2.0.'
-                .' Use Ergonode\Core\Application\HttpFoundation\FileContentResponse instead.',
-            \E_USER_DEPRECATED,
-        );
         $headers = [
             'Cache-Control' => 'private',
             'Content-type' => $storage->getMimetype($filename),
             'Content-Disposition' => 'attachment; filename="'.basename($filename).'";',
             'Content-length' => $storage->getSize($filename),
         ];
-        parent::__construct($storage->read($filename), Response::HTTP_OK, $headers);
+        $content = $storage->read($filename);
+
+        if (false === $content) {
+            throw new \LogicException('There\'s no such file');
+        }
+
+        parent::__construct($content, Response::HTTP_OK, $headers);
     }
 }
